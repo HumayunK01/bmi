889e075edb528f15929de6d7bf8899bc0c0ec977
Humayun Khan <humayunk.pvt@gmail.com>
2023-02-22 01:17:38 +0530

    init

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-  
+function calculateBMI(){
+    var weight = parseInt(document.getElementById('weight').value);
+    var height = parseInt(document.getElementById('height').value);
+    var bmi = weight / ((height / 100) ** 2);
+    var result = document.getElementById('result');
+
+    if(bmi < 18.5){
+        result.innerHTML = "Your BMI is " + bmi.toFixed(1) + ". You're underweight."
+    }
+    else if (bmi >= 18.5 && bmi < 25) {
+        result.innerHTML = "Your BMI is " + bmi.toFixed(1) + ". You have a normal weight.";
+    } 
+    else if (bmi >= 25 && bmi < 30) {
+        result.innerHTML = "Your BMI is " + bmi.toFixed(1) + ". You are overweight.";
+    } 
+    else {
+        result.innerHTML = "Your BMI is " + bmi.toFixed(1) + ". You are obese.";
+    }
+}  
